a3a0e3d7a12d0e0673fc4bee3778bc3e165e54cd
Alexey Kosmachev <kosmos@neatsy.ai>
2021-03-18 01:47:51 +0300

    Check solution function

--- autochecker/core/scorer/gateway.py
+++ autochecker/core/scorer/gateway.py
@@ -1,8 +1,11 @@
+from concurrent.futures.thread import ThreadPoolExecutor
+from functools import partial
 from importlib.resources import path
 from typing import Tuple, Optional, List
 
 import pandas as pd
 import requests
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 
@@ -18,7 +21,7 @@ class MnistHTTPScorer:
             df = pd.read_csv(dataset_path)
             y = df['class'].values
             X = df.drop(['class'], axis=1).values
-            _, X_test, _, y_test = train_test_split(X, y, test_size=0.3, random_state=789)
+            _, X_test, _, y_test = train_test_split(X, y, test_size=0.1, random_state=789)
 
         return X_test.tolist(), y_test.tolist()
 
@@ -36,13 +39,12 @@ class MnistHTTPScorer:
             return -1, str(e)
 
     def check_solution(self, service_url: str) -> Tuple[float, List[str]]:
-        return 0.3, ['biba', 'kuka']
-        # with ThreadPoolExecutor(max_workers=20) as executor:
-        #     result = executor.map(partial(self._check_one_example, service_url=service_url), self._X_test)
-        #
-        # y_pred, errors = list(zip(*result))
-        #
-        # y_true = self._y_test
-        # accuracy = accuracy_score(y_pred, y_true)
-        #
-        # return accuracy, errors
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            result = executor.map(partial(self._check_one_example, service_url=service_url), self._X_test)
+
+        y_pred, errors = list(zip(*result))
+
+        y_true = self._y_test
+        accuracy = accuracy_score(y_pred, y_true)
+
+        return accuracy, [x for x in errors if x]
